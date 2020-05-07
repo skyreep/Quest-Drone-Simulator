8f90f08d87094bd431fbce2e4282f9bf106dcecd
Skyreep <skyreep@gmail.com>
2020-05-07 09:49:30 -0400

    Uploaded logic scripts

---
+++ RotateDroneCam.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateDroneCam : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Spin the object around the world origin at 20 degrees/second.
+        transform.RotateAround(target.transform.position, Vector3.up, speed * Time.deltaTime);
+        //transform.Rotate(Vector3.up, 1 * speed * Time.deltaTime);
+    }
+}
---
+++ WeaponFire.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponFire : MonoBehaviour
+{
+    public float thrust = 1.0f;
+    public Rigidbody rb;
+    public GameObject dronePrefab;
+    //public GameObject projectile;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
+        Physics.IgnoreCollision(dronePrefab.GetComponent<Collider>(), GetComponent<Collider>());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            //Instantiate(projectile);
+            //projectile.transform.SetParent(dronePrefab.transform);
+            rb.useGravity = true;
+            rb.AddRelativeForce(0, thrust, 0, ForceMode.Impulse);
+        }
+    }
+
+
+}
